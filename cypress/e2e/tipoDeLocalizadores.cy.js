b0ff7a226263407705802ccf228d277b50879428
Chamorro <abner.chamorro@softwareone.com>
2024-02-10 18:58:33 -0500

    Utilizar contains

--- cypress/e2e/tipoDeLocalizadores.cy.js
+++ cypress/e2e/tipoDeLocalizadores.cy.js
@@ -1,12 +1,40 @@
 describe("Tipos de localizadores", () => {
-    it("Obtenerlo por medio de un tag", () => {
+    it("Obtenerlo por medio de un atributo", () => {
         cy.visit("/automation-practice-form")
         cy.get("input")
     })
 
-    it("Obtenerlo por medio de un atributo", () => {
+    it("Obtenerlo por medio de un tag", () => {
         // cy.visit("/automation-practice-form")
         cy.get('[placeholder="First Name"]')
     })
+
+
+    it("Obtenerlo por medio de un atributo y un tag", () => {
+        cy.get('input[placeholder="First Name"]')
+    })
+
+    it("Obtenerlo por medio de un id", () => {
+        cy.get("#firstName")
+    })
+
+    it("Obtenerlo por medio de una clase", () => {
+        cy.get(".mr-sm-2.form-control")
+    })
+
+    it("Usando contains", () => {
+        cy.contains("Reading")
+        cy.contains(".element-group", "Widgets")
+    })
+
+    it("Usando parent", () => {
+        //Obteniendo el elemento padre
+        cy.get('input[placeholder="First Name"]').parent()
+        //Obteniendo los elementos padres
+        cy.get('input[placeholder="First Name"]').parents()
+        cy.get('input[placeholder="First Name"]').parents().find('label')
+        cy.get("form").find("label")
+    })
+
 
 })
